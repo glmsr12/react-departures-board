9b33f5d8b591baece7581daa82bd9cd2613afbf0
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-14 10:18:25 -0700

    "backend files created"

---
+++ server.js
@@ -0,0 +1,10 @@
+const PORT = 800;
+const axios = require('axios').default;
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+
+app.listen(PORT, () => console.log('running on port' + PORT));
