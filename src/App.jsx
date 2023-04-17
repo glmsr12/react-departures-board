2e397364f9875552bbddd9fa9fbfdb1a671923e4
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-16 22:11:52 -0700

    "update style"

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div>
+      <header className="departures">DEPARTURES</header>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
