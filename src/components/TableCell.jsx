f7f3460612fad7d52f57957063ef28e1d9bc4fa0
glmsr12 <gulumsercakmak@hotmail.com>
2023-04-20 02:46:59 -0700

    "update style "

--- src/components/TableCell.jsx
+++ src/components/TableCell.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 
-function TableCell() {
-  return <div></div>;
+function TableCell({ word, index }) {
+  const [flip, setFlip] = useState(false);
+
+  setTimeout(() => {
+    setFlip(true);
+  }, 100 * index);
+
+  return (
+    <td>
+      {Array.from(word).map((letter, _index) => (
+        <div className={flip ? 'flip' : null}>{flip ? letter : ''}</div>
+      ))}
+    </td>
+  );
 }
 
 export default TableCell;
